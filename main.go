889839979924378b1fbd734a68fa6e6a7828b6ee
Marc CB <marc.cbrooks@gmail.com>
2017-11-29 10:37:59 -0500

    add basic server

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// create new router instance
+	r := mux.NewRouter()
+
+	// serve static files
+	r.Handle("/", http.FileServer(http.Dir("./static/")))
+	r.PathPrefix("/dist/").Handler(http.FileServer(http.Dir("./static/")))
+
+	// start server
+	log.Fatal(http.ListenAndServe(":6000", r))
+}
