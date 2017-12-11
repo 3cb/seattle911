8da397956a67fd51afab65d23b6efb8ad52bf3ff
Marc C-B <marc.cbrooks@gmail.com>
2017-12-11 11:14:42 -0500

    add database initialization

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/boltdb/bolt"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	// open database and create buckets of key/value pairs
+	db, err := bolt.Open("fire.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = createBucket(db, "Fire")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = createBucket(db, "Police")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	// create new router instance
 	r := mux.NewRouter()
 
@@ -17,4 +34,14 @@ func main() {
 
 	// start server
 	log.Fatal(http.ListenAndServe(":3030", r))
+}
+
+func createBucket(db *bolt.DB, name string) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		_, err2 := tx.CreateBucketIfNotExists([]byte(name))
+		if err2 != nil {
+			return fmt.Errorf("Error creating bucket: %s", err2)
+		}
+		return nil
+	})
 }
