58a396e2419dbeb489556de10c6fced2d759cd8d
Marc CB <marc.cbrooks@gmail.com>
2017-12-18 23:31:16 -0500

    add wsHandler

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/websocket"
+
 	"github.com/3cb/ssc"
 
 	"github.com/boltdb/bolt"
@@ -28,7 +30,7 @@ func main() {
 		IsWritable: true,
 		IsJSON:     false,
 	}
-	wsp, err := ssc.NewSocketPool(nil, config)
+	wsp, err := ssc.NewSocketPool(config)
 	if err != nil {
 		log.Fatal("Unable to start pool to serve websocket connections.")
 	}
@@ -43,6 +45,25 @@ func main() {
 	r.Handle("/", http.FileServer(http.Dir("./static/")))
 	r.PathPrefix("/dist/").Handler(http.FileServer(http.Dir("./static/")))
 
+	// handle websocket requests
+	upgrader := &websocket.Upgrader{}
+	r.Handle("/ws", wsHandler(wsp, upgrader))
+
 	// start server
 	log.Fatal(http.ListenAndServe(":3030", r))
+}
+
+func wsHandler(pool *ssc.SocketPool, upgrader *websocket.Upgrader) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Printf("Unable to connect to new client via websocket.")
+			return
+		}
+		s := ssc.NewSocketInstance("", pool.Config)
+		s.Connection = conn
+		pool.ReadStack[s] = true
+		pool.WriteStack[s] = true
+		log.Printf("New client connected via websocket.")
+	})
 }
