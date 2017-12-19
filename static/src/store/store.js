e90ad40f358b89cd2c4318d5ef49160bd359e237
Marc CB <marc.cbrooks@gmail.com>
2017-12-19 15:16:05 -0500

    add websocket to store

--- static/src/store/store.js
+++ static/src/store/store.js
@@ -5,9 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-
+        ws: null,
+        wsConnected: false
     },
     mutations: {
-        
+        startWS(state) {
+            state.ws = new WebSocket("ws://localhost:3030/ws")
+            state.ws.onopen = event => {
+                state.wsConnected = true
+            }
+        }
     }
 })
