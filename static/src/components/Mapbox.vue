98417b8ac0c5b2902b445f444bb28623128dbc0e
Marc CB <marc.cbrooks@gmail.com>
2017-12-19 15:16:34 -0500

    add producer, main stream, and main listener

--- static/src/components/Mapbox.vue
+++ static/src/components/Mapbox.vue
@@ -3,66 +3,102 @@
 </template>
 
 <script>
+import xs from 'xstream'
+
 export default {
-    mounted() {
-        mapboxgl.accessToken = 'pk.eyJ1IjoibWFyY2NiIiwiYSI6ImNqYTR1enN2dGE0bWEyd3BhcTd6cnBzc3MifQ.Z4zYRzVCXv5zCqqdpgKZ-w';
-        var map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v10',
-            center: [-122.335167, 47.608013],
-            zoom: 11
-        });
-
-        map.on('load', function () {
-
-    map.addLayer({
-        "id": "points",
-        "type": "symbol",
-        "source": {
-            "type": "geojson",
-            "data": {
-                "type": "FeatureCollection",
-                "features": [{
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [-77.03238901390978, 38.913188059745586]
-                    },
-                    "properties": {
-                        "title": "Mapbox DC",
-                        "icon": "monument"
-                    }
-                }, {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [-122.414, 37.776]
-                    },
-                    "properties": {
-                        "title": "Mapbox SF",
-                        "icon": "harbor"
-                    }
-                }]
-            }
-        },
-        "layout": {
-            "icon-image": "{icon}-15",
-            "text-field": "{title}",
-            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-            "text-offset": [0, 0.6],
-            "text-anchor": "top"
-        }
+  data() {
+      return {
+          producer: {
+              start: (listener) => {
+                  this.$store.commit('startWS')
+                  this.$store.state.ws.onmessage = event => {
+                      listener.next(event)
+                  }
+              },
+              stop: () => {
+                  console.log("No longer listening to websocket.")
+              }
+          },
+          mainListener: {
+              next: value => {
+                  
+              },
+              error: err => {
+                  console.error("Error on main stream: ", err)
+              },
+              close: () => {
+                  console.log("No longer listening to main stream.")
+              }
+          }
+      }
+  },
+  computed: {
+      main$() {
+          return xs.CreateWithMemory(this.producer)
+      }
+  },
+  mounted() {
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoibWFyY2NiIiwiYSI6ImNqYTR1enN2dGE0bWEyd3BhcTd6cnBzc3MifQ.Z4zYRzVCXv5zCqqdpgKZ-w";
+    var map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v10",
+      center: [-122.335167, 47.608013],
+      zoom: 11
     });
-});
-    }
-}
+
+    map.on("load", function() {
+      map.addLayer({
+        id: "points",
+        type: "symbol",
+        source: {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: [
+              {
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: [-77.03238901390978, 38.913188059745586]
+                },
+                properties: {
+                  title: "Mapbox DC",
+                  icon: "monument"
+                }
+              },
+              {
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: [-122.414, 37.776]
+                },
+                properties: {
+                  title: "Mapbox SF",
+                  icon: "harbor"
+                }
+              }
+            ]
+          }
+        },
+        layout: {
+          "icon-image": "{icon}-15",
+          "text-field": "{title}",
+          "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+          "text-offset": [0, 0.6],
+          "text-anchor": "top"
+        }
+      });
+    });
+  }
+};
 </script>
 
 <style>
 #map {
-    position:absolute;
-    top:0;
-    bottom:0;
-    width:100%
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
 }
 </style>
