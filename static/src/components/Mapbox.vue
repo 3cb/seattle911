51a011532ce75148df6cc086cc6b3f49343b1102
Marc CB <marc.cbrooks@gmail.com>
2017-12-20 22:06:31 -0500

    add mutation to update state with data from flatbuffer

--- static/src/components/Mapbox.vue
+++ static/src/components/Mapbox.vue
@@ -3,41 +3,52 @@
 </template>
 
 <script>
-import xs from 'xstream'
+import xs from "xstream";
+var flatbuffers = require("../../node_modules/flatbuffers").flatbuffers;
+var seattle = require("../seattle/schema_generated.js").seattle;
 
 export default {
   data() {
-      return {
-          producer: {
-              start: (listener) => {
-                  this.$store.commit('startWS')
-                  this.$store.state.ws.onmessage = event => {
-                      listener.next(event)
-                  }
-              },
-              stop: () => {
-                  console.log("No longer listening to websocket.")
-              }
-          },
-          mainListener: {
-              next: value => {
-                  
-              },
-              error: err => {
-                  console.error("Error on main stream: ", err)
-              },
-              close: () => {
-                  console.log("No longer listening to main stream.")
-              }
-          }
+    return {
+      producer: {
+        start: listener => {
+          this.$store.commit("startWS");
+          this.$store.state.ws.onmessage = event => {
+            let bytes = new Uint8Array(event.data);
+            let buf = new flatbuffers.ByteBuffer(bytes);
+            let message = seattle.Message.getRootAsMessage(buf);
+            listener.next(message);
+          };
+        },
+        stop: () => {
+          console.log("No longer listening to websocket.");
+        }
+      },
+      updateListener: {
+        next: message => {
+          console.log(message);
+          this.$store.commit("updateCalls", message);
+        },
+        error: err => {
+          console.error("Error on main stream: ", err);
+        },
+        close: () => {
+          console.log("No longer listening to main stream.");
+        }
       }
+    };
   },
   computed: {
-      main$() {
-          return xs.CreateWithMemory(this.producer)
-      }
+    main$() {
+      return xs.createWithMemory(this.producer);
+    },
+    update$() {
+      return xs.from(this.main$);
+    }
   },
   mounted() {
+    this.main$.addListener(this.updateListener);
+
     mapboxgl.accessToken =
       "pk.eyJ1IjoibWFyY2NiIiwiYSI6ImNqYTR1enN2dGE0bWEyd3BhcTd6cnBzc3MifQ.Z4zYRzVCXv5zCqqdpgKZ-w";
     var map = new mapboxgl.Map({
--- static/src/store/store.js
+++ static/src/store/store.js
@@ -6,14 +6,68 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         ws: null,
-        wsConnected: false
+        wsConnected: false,
+
+        calls: {
+            today: {
+                fire: [], // { address, datetime, incident_number, latitude, longitude, type }
+                police: [] // { at_scene_time, cad_cdw_id, cad_event_number, census_tract, district_sector, event_clearance_coe, event_clearance_date, event_clearance_description, event_clearance_group, event_clearance_subgroup, general_offense_number, hundred_block_location, initial_type_description, initial_type_group, initial_type_subgroup, latitude, longitude, zone_beat }
+            },
+            history: {
+                fire: [],
+                police: []
+            }
+        }
     },
     mutations: {
         startWS(state) {
             state.ws = new WebSocket("ws://localhost:3030/ws")
+            state.ws.binaryType = 'arraybuffer'
             state.ws.onopen = event => {
+                console.log(event)
                 state.wsConnected = true
             }
+        },
+        updateCalls(state, msg) {
+            state.calls.today.fire = []
+            var lenFire = msg.fireCallsLength()
+            for (let i = 0; i < lenFire; i++) {
+                state.calls.today.fire.push({
+                    address: msg.fireCalls(i).address(),
+                    datetime: msg.fireCalls(i).datetime(),
+                    incidentNumber: msg.fireCalls(i).incidentNumber(),
+                    latitude: msg.fireCalls(i).latitude(),
+                    longitude: msg.fireCalls(i).longitude(),
+                    type: msg.fireCalls(i).type()
+                })
+            }
+            console.log(state.calls.today.fire)
+
+            state.calls.today.police = []
+            var lenPolice = msg.policeCallsLength()
+            for (let i = 0; i < lenPolice; i++) {
+                state.calls.today.police.push({
+                    atSceneTime: msg.policeCalls(i).atSceneTime(),
+                    cadCdwId: msg.policeCalls(i).cadCdwId(),
+                    cadEventNumber: msg.policeCalls(i).cadEventNumber(),
+                    censusTract: msg.policeCalls(i).censusTract(),
+                    districtSector: msg.policeCalls(i).districtSector(),
+                    eventClearanceCode: msg.policeCalls(i).eventClearanceCode(),
+                    eventClearanceDate: msg.policeCalls(i).eventClearanceDate(),
+                    eventClearanceDescription: msg.policeCalls(i).eventClearanceDescription(),
+                    eventClearanceGroup: msg.policeCalls(i).eventClearanceGroup(),
+                    eventClearanceSubgroup: msg.policeCalls(i).eventClearanceSubgroup(),
+                    generalOffenseNumber: msg.policeCalls(i).generalOffenseNumber(),
+                    hundredBlockLocation: msg.policeCalls(i).hundredBlockLocation(),
+                    initialTypeDescription: msg.policeCalls(i).initialTypeDescription(),
+                    initialTypeGroup: msg.policeCalls(i).initialTypeGroup(),
+                    initialTypeSubgroup: msg.policeCalls(i).initialTypeSubgroup(),
+                    latitude: msg.policeCalls(i).latitude(),
+                    longitude: msg.policeCalls(i).longitude(),
+                    zoneBeat: msg.policeCalls(i).zoneBeat()
+                })
+            }
+            console.log(state.calls.today.police)
         }
     }
 })
