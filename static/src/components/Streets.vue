184a16e16a894a8bffb0636dba832117429d2d41
Marc CB <marc.cbrooks@gmail.com>
2017-12-26 22:33:31 -0500

    add popups to streets map

--- static/src/components/Streets.vue
+++ static/src/components/Streets.vue
@@ -5,8 +5,8 @@
 <script>
 var flatbuffers = require("../../node_modules/flatbuffers").flatbuffers;
 var seattle = require("../seattle/schema_generated.js").seattle;
-import axios from 'axios'
-import moment from 'moment'
+import axios from "axios";
+import moment from "moment";
 
 export default {
   data() {
@@ -19,22 +19,22 @@ export default {
       return this.$store.state.features.fire;
     },
     pfeatures() {
-        return this.$store.state.features.police;
+      return this.$store.state.features.police;
     }
   },
   watch: {
-      ffeatures(fft) {
-        this.map.getSource("fcalls").setData({
-          "type": "FeatureCollection",
-          "features": fft
-        });
-      },
-      pfeatures(pft) {
-          this.map.getSource("pcalls").setData({
-              "type": "FeatureCollection",
-              "features": pft
-          })
-      }
+    ffeatures(fft) {
+      this.map.getSource("fcalls").setData({
+        type: "FeatureCollection",
+        features: fft
+      });
+    },
+    pfeatures(pft) {
+      this.map.getSource("pcalls").setData({
+        type: "FeatureCollection",
+        features: pft
+      });
+    }
   },
   mounted() {
     mapboxgl.accessToken =
@@ -43,59 +43,109 @@ export default {
       container: "map",
       style: "mapbox://styles/mapbox/streets-v10",
       center: [-122.335167, 47.608013],
-      zoom: 11
+      zoom: 10.75
     });
+    // Add zoom and rotation controls to the map.
+    this.map.addControl(new mapboxgl.NavigationControl());
 
-    this.map.on('load', () => {
-        axios({
-            url: '/day/' + moment().format().split('T')[0],
-                method: 'get',
-                responseType: 'arraybuffer'
-            })
-            .then(response => {
-                let bytes = new Uint8Array(response.data);
-                let buf = new flatbuffers.ByteBuffer(bytes);
-                let message = seattle.Message.getRootAsMessage(buf);
-                this.$store.commit('updateCalls', message)
-                this.$store.commit("updateFeatures");
-                this.map.addSource('fcalls', {
-                    "type": "geojson",
-                    "data": {
-                        "type": "FeatureCollection",
-                        "features": this.ffeatures
-                    }
-                })
-                this.map.addSource('pcalls', {
-                    "type": "geojson",
-                    "data": {
-                        "type": "FeatureCollection",
-                        "features": this.pfeatures
-                    }
-                })
-                this.map.addLayer({
-                    "id": "fire",
-                    "type": "circle",
-                    "source": "fcalls",
-                    "paint": {
-                        "circle-radius": 6,
-                        "circle-color": "#B42222"
-                    }
-                });
-                this.map.addLayer({
-                    "id": "police",
-                    "type": "circle",
-                    "source": "pcalls",
-                    "paint": {
-                        "circle-radius": 6,
-                        "circle-color": "#034cc1"
-                    }
-                    
-                })
-            })
-            .catch(error => {
-                console.error(error)
-            })
-    })
+    this.map.on("load", () => {
+      axios({
+        url:
+          "/api/day/" +
+          moment()
+            .format()
+            .split("T")[0],
+        method: "get",
+        responseType: "arraybuffer"
+      })
+        .then(response => {
+          let bytes = new Uint8Array(response.data);
+          let buf = new flatbuffers.ByteBuffer(bytes);
+          let message = seattle.Message.getRootAsMessage(buf);
+          this.$store.commit("updateCalls", message);
+          this.$store.commit("updateFeatures");
+
+          this.map.addSource("fcalls", {
+            type: "geojson",
+            data: {
+              type: "FeatureCollection",
+              features: this.ffeatures
+            }
+          });
+          this.map.addSource("pcalls", {
+            type: "geojson",
+            data: {
+              type: "FeatureCollection",
+              features: this.pfeatures
+            }
+          });
+          this.map.addLayer({
+            id: "fire",
+            type: "circle",
+            source: "fcalls",
+            paint: {
+              "circle-radius": 6,
+              "circle-color": "#B42222"
+            }
+          });
+          this.map.addLayer({
+            id: "police",
+            type: "circle",
+            source: "pcalls",
+            paint: {
+              "circle-radius": 6,
+              "circle-color": "#034cc1"
+            }
+          });
+
+          var popupFire = new mapboxgl.Popup({
+            closeButton: false,
+            closeOnClick: false
+          });
+
+          this.map.on("mouseenter", "fire", (e) => {
+            // Change the cursor style as a UI indicator.
+            this.map.getCanvas().style.cursor = "pointer";
+
+            // Populate the popup and set its coordinates
+            // based on the feature found.
+            popupFire
+              .setLngLat(e.features[0].geometry.coordinates)
+              .setHTML(e.features[0].properties.description)
+              .addTo(this.map);
+          });
+
+          this.map.on("mouseleave", "fire", () => {
+            this.map.getCanvas().style.cursor = "";
+            popupFire.remove();
+          });
+
+          var popupPolice = new mapboxgl.Popup({
+            closeButton: false,
+            closeOnClick: false
+          });
+
+          this.map.on("mouseenter", "police", (e) => {
+            // Change the cursor style as a UI indicator.
+            this.map.getCanvas().style.cursor = "pointer";
+
+            // Populate the popup and set its coordinates
+            // based on the feature found.
+            popupPolice
+              .setLngLat(e.features[0].geometry.coordinates)
+              .setHTML(e.features[0].properties.description)
+              .addTo(this.map);
+          });
+
+          this.map.on("mouseleave", "police", () => {
+            this.map.getCanvas().style.cursor = "";
+            popupPolice.remove();
+          });
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    });
   }
 };
 </script>
@@ -106,5 +156,10 @@ export default {
   top: 0;
   bottom: 0;
   width: 100%;
+}
+
+.mapboxgl-popup {
+  max-width: 400px;
+  font: 12px/20px "Helvetica Neue", Arial, Helvetica, sans-serif;
 }
 </style>
--- static/src/store/store.js
+++ static/src/store/store.js
@@ -4,98 +4,104 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-        ws: null,
-        wsConnected: false,
+  state: {
+    ws: null,
+    wsConnected: false,
 
-        calls: {
-            today: {
-                fire: [], // { address, datetime, incident_number, latitude, longitude, type }
-                police: [] // { at_scene_time, cad_cdw_id, cad_event_number, census_tract, district_sector, event_clearance_coe, event_clearance_date, event_clearance_description, event_clearance_group, event_clearance_subgroup, general_offense_number, hundred_block_location, initial_type_description, initial_type_group, initial_type_subgroup, latitude, longitude, zone_beat }
-            },
-            history: {
-                fire: [],
-                police: []
-            }
-        },
-
-        features: {
-            fire: [],
-            police: []
-        }
+    calls: {
+      today: {
+        fire: [], // { address, datetime, incident_number, latitude, longitude, type }
+        police: [] // { at_scene_time, cad_cdw_id, cad_event_number, census_tract, district_sector, event_clearance_coe, event_clearance_date, event_clearance_description, event_clearance_group, event_clearance_subgroup, general_offense_number, hundred_block_location, initial_type_description, initial_type_group, initial_type_subgroup, latitude, longitude, zone_beat }
+      },
+      history: {
+        fire: [],
+        police: []
+      }
     },
-    mutations: {
-        startWS(state) {
-            state.ws = new WebSocket("ws://localhost:3030/ws")
-            state.ws.binaryType = 'arraybuffer'
-            state.ws.onopen = event => {
-                state.wsConnected = true
-            }
-        },
-        updateCalls(state, msg) {
-            state.calls.today.fire = []
-            var lenFire = msg.fireCallsLength()
-            for (let i = 0; i < lenFire; i++) {
-                state.calls.today.fire.push({
-                    address: msg.fireCalls(i).address(),
-                    datetime: msg.fireCalls(i).datetime(),
-                    incidentNumber: msg.fireCalls(i).incidentNumber(),
-                    latitude: msg.fireCalls(i).latitude(),
-                    longitude: msg.fireCalls(i).longitude(),
-                    type: msg.fireCalls(i).type()
-                })
-            }
-            console.log("fire", state.calls.today.fire)
 
-            state.calls.today.police = []
-            var lenPolice = msg.policeCallsLength()
-            for (let i = 0; i < lenPolice; i++) {
-                state.calls.today.police.push({
-                    atSceneTime: msg.policeCalls(i).atSceneTime(),
-                    cadCdwId: msg.policeCalls(i).cadCdwId(),
-                    cadEventNumber: msg.policeCalls(i).cadEventNumber(),
-                    censusTract: msg.policeCalls(i).censusTract(),
-                    districtSector: msg.policeCalls(i).districtSector(),
-                    eventClearanceCode: msg.policeCalls(i).eventClearanceCode(),
-                    eventClearanceDate: msg.policeCalls(i).eventClearanceDate(),
-                    eventClearanceDescription: msg.policeCalls(i).eventClearanceDescription(),
-                    eventClearanceGroup: msg.policeCalls(i).eventClearanceGroup(),
-                    eventClearanceSubgroup: msg.policeCalls(i).eventClearanceSubgroup(),
-                    generalOffenseNumber: msg.policeCalls(i).generalOffenseNumber(),
-                    hundredBlockLocation: msg.policeCalls(i).hundredBlockLocation(),
-                    initialTypeDescription: msg.policeCalls(i).initialTypeDescription(),
-                    initialTypeGroup: msg.policeCalls(i).initialTypeGroup(),
-                    initialTypeSubgroup: msg.policeCalls(i).initialTypeSubgroup(),
-                    latitude: msg.policeCalls(i).latitude(),
-                    longitude: msg.policeCalls(i).longitude(),
-                    zoneBeat: msg.policeCalls(i).zoneBeat()
-                })
-            }
-            console.log("police", state.calls.today.police)
-        },
-        updateFeatures(state) {
-            state.features.fire = []
-            for (let i = 0; i < state.calls.today.fire.length; i++) {
-                state.features.fire.push({
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [parseFloat(state.calls.today.fire[i].longitude), parseFloat(state.calls.today.fire[i].latitude)]
-                    }
-                })
-            }
-
-            state.features.police = []
-            for (let i = 0; i < state.calls.today.police.length; i++) {
-                state.features.police.push({
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [parseFloat(state.calls.today.police[i].longitude), parseFloat(state.calls.today.police[i].latitude)]
-                    }
-                })
-            }
-            console.log("features", state.features)
-        }
+    features: {
+      fire: [],
+      police: []
     }
+  },
+  mutations: {
+    startWS(state) {
+      state.ws = new WebSocket("ws://localhost:3030/ws")
+      state.ws.binaryType = 'arraybuffer'
+      state.ws.onopen = event => {
+        state.wsConnected = true
+      }
+    },
+    updateCalls(state, msg) {
+      state.calls.today.fire = []
+      var lenFire = msg.fireCallsLength()
+      for (let i = 0; i < lenFire; i++) {
+        state.calls.today.fire.push({
+          address: msg.fireCalls(i).address(),
+          datetime: msg.fireCalls(i).datetime(),
+          incidentNumber: msg.fireCalls(i).incidentNumber(),
+          latitude: msg.fireCalls(i).latitude(),
+          longitude: msg.fireCalls(i).longitude(),
+          type: msg.fireCalls(i).type()
+        })
+      }
+      console.log("fire", state.calls.today.fire)
+
+      state.calls.today.police = []
+      var lenPolice = msg.policeCallsLength()
+      for (let i = 0; i < lenPolice; i++) {
+        state.calls.today.police.push({
+          atSceneTime: msg.policeCalls(i).atSceneTime(),
+          cadCdwId: msg.policeCalls(i).cadCdwId(),
+          cadEventNumber: msg.policeCalls(i).cadEventNumber(),
+          censusTract: msg.policeCalls(i).censusTract(),
+          districtSector: msg.policeCalls(i).districtSector(),
+          eventClearanceCode: msg.policeCalls(i).eventClearanceCode(),
+          eventClearanceDate: msg.policeCalls(i).eventClearanceDate(),
+          eventClearanceDescription: msg.policeCalls(i).eventClearanceDescription(),
+          eventClearanceGroup: msg.policeCalls(i).eventClearanceGroup(),
+          eventClearanceSubgroup: msg.policeCalls(i).eventClearanceSubgroup(),
+          generalOffenseNumber: msg.policeCalls(i).generalOffenseNumber(),
+          hundredBlockLocation: msg.policeCalls(i).hundredBlockLocation(),
+          initialTypeDescription: msg.policeCalls(i).initialTypeDescription(),
+          initialTypeGroup: msg.policeCalls(i).initialTypeGroup(),
+          initialTypeSubgroup: msg.policeCalls(i).initialTypeSubgroup(),
+          latitude: msg.policeCalls(i).latitude(),
+          longitude: msg.policeCalls(i).longitude(),
+          zoneBeat: msg.policeCalls(i).zoneBeat()
+        })
+      }
+      console.log("police", state.calls.today.police)
+    },
+    updateFeatures(state) {
+      state.features.fire = []
+      for (let i = 0; i < state.calls.today.fire.length; i++) {
+        state.features.fire.push({
+          "type": "Feature",
+          "properties": {
+            "description": "<strong>" + state.calls.today.fire[i].type.toUpperCase() + "</strong><p>Time: " + state.calls.today.fire[i].datetime.split("T")[1].split(".")[0] + "</p><p>Date: " + state.calls.today.fire[i].datetime.split("T")[0] + "</p><p>Location: " + state.calls.today.fire[i].address + "</p><p>Incident #: " + state.calls.today.fire[i].incidentNumber + "</p>"
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [parseFloat(state.calls.today.fire[i].longitude), parseFloat(state.calls.today.fire[i].latitude)]
+          }
+        })
+      }
+
+      state.features.police = []
+      for (let i = 0; i < state.calls.today.police.length; i++) {
+        state.features.police.push({
+          "type": "Feature",
+          "properties": {
+              "description": "<strong>" + state.calls.today.police[i].initialTypeDescription.toUpperCase() + "</strong><p>At Scene Time: " + state.calls.today.police[i].atSceneTime.split("T")[1].split(".")[0] + "</p><p>Date: " + state.calls.today.police[i].atSceneTime.split("T")[0] + "</p><p>Location: " + state.calls.today.police[i].hundredBlockLocation + "</p><p>Event #: " + state.calls.today.police[i].cadEventNumber + "</p>"
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [parseFloat(state.calls.today.police[i].longitude), parseFloat(state.calls.today.police[i].latitude)]
+          }
+        })
+      }
+      console.log("features", state.features)
+    }
+  }
 })
