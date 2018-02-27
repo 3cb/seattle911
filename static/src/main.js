e98e2bdd4f44ee1d8e6ec7972fff59bdd6c18e0e
Marc CB <marc.cbrooks@gmail.com>
2018-02-26 20:05:49 -0500

    import ElementUI

--- static/src/main.js
+++ static/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import 'bulma/css/bulma.css'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+import 'element-ui/lib/theme-chalk/index.css'
 import Vuex from 'vuex'
 import App from './App.vue'
 import store from './store/store.js'
+
+Vue.use(ElementUI, {
+  locale
+})
 
 new Vue({
   el: '#app',
